726016ac26ceea25d0e6301dc63f68e113015bf8
Matthew Shin <mshin1995@gmail.com>
2019-02-20 19:18:00 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,5 +16,20 @@ class Book
     @author
   end
   
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+    
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
 end
 
